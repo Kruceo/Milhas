a402a8a8c451a737929155703065c44e77a1d2fd
Kruceo <rafa2015krueger@gmail.com>
2023-03-13 17:10:32 -0300

    init

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import './app.css'
+import { Path } from './assets/test'
+
+export function App() {
+
+  return (
+    <>
+      <Path as="/local/$country">
+        <h2>rafola</h2>
+      </Path>
+    </>
+  )
+}

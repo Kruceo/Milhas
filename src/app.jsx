252adf2f485b1595db6362ce6c7182f8408d688c
Kruceo <rafa2015krueger@gmail.com>
2023-03-13 17:28:29 -0300

    att: init implement path absolute passthrought

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,18 @@
 import './app.css'
-import { Path } from './assets/test'
+import { Path } from '../../Path'
 
 export function App() {
 
   return (
     <>
-      <Path as="/local/$country">
-        <h2>rafola</h2>
+      <Path as="/local/$country" absolute>
+        <div>choose a pão de forma</div>
+
+      </Path>
+      <Path as="/local/$country/panel" absolute>
+        <h2>panel</h2>
+        <h2>option1</h2>
+        <h2>option2</h2>
       </Path>
     </>
   )
